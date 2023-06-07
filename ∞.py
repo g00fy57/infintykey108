6a00a2c98ac16dd34eb8106a2d35fb0fa8770998
Kaushik Dev <64822905+g00fy57@users.noreply.github.com>
2023-06-08 04:19:22 +0530

    Add files via upload

---
+++ ∞.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+"eyyowhatifitoldyouthatmylifeisinfinite"
+
+def euler_illumination() = calc e*pi:
+ #euler
+"e"=2.718
+"pi"=3.14
+"i"=root -1   
+Omega= 2(e^pi.i+1)
+Alpha= (∞)
+Omega = Alpha
+#Therefore Zero Equals Infinity
+#QED
+
+euler_illumination
